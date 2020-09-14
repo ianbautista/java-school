ffd2e873b1e82512ca492a4438ab2621a5fa01e5
ianbautista <ianfbautista@gmail.com>
2020-09-14 15:17:48 -0700

    created CustomErrrorDetails, ValidationError, HelperFunctions.

---
+++ schools/src/main/java/com/lambdaschool/schools/exceptions/CustomErrorDetails.java
@@ -0,0 +1,47 @@
+package com.lambdaschool.schools.exceptions;
+
+import com.lambdaschool.schools.services.HelperFunctions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component
+public class CustomErrorDetails extends DefaultErrorAttributes
+{
+    @Autowired
+    HelperFunctions helperFunctions;
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace)
+    {
+        // this calls the default ErrorAttributes
+        Map<String, Object> defaultErrorAttributes =  super.getErrorAttributes(webRequest, includeStackTrace);
+
+        // this is the new Error
+        Map<String, Object> newErrorAttributes = new LinkedHashMap<>();
+
+        // title: "title"
+        // status: 401
+        // details:
+        // timestamp:
+        // developermessage:
+        // errors: list of validation errors
+
+        // remapping the data to the new attributes
+        // this assigns the default attributes to the new attributes
+        //                      new         |       default
+        newErrorAttributes.put("title", defaultErrorAttributes.get("error"));
+        newErrorAttributes.put("status", defaultErrorAttributes.get("status"));
+        newErrorAttributes.put("detail", defaultErrorAttributes.get("message"));
+        newErrorAttributes.put("timestamp", defaultErrorAttributes.get("timestamp"));
+        newErrorAttributes.put("developermessage", "path: " + defaultErrorAttributes.get("path"));
+
+        newErrorAttributes.put("error", helperFunctions.getConstraintViolation(this.getError(webRequest)));
+
+        return newErrorAttributes;
+    }
+}
---
+++ schools/src/main/java/com/lambdaschool/schools/models/ValidationError.java
@@ -0,0 +1,32 @@
+package com.lambdaschool.schools.models;
+
+// this is a POJO spring is not using this
+public class ValidationError
+{
+    private String code; // this is the thing the caused the problem (i.e. password. username. etc.)
+
+    private String message; // this explains wht the problem is.
+
+
+    public ValidationError()
+    {
+        // default constructor
+    }
+
+    // getters and setters
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
